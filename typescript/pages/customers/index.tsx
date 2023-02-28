346d7bfa3f68cc61c8be94eb52c226fc3d6544be
Eniekpo <johneniekpo4life@gmail.com>
2023-02-28 16:25:50 +0100

    a

---
+++ typescript/pages/customers/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Customers: NextPage = () => {
+    return <h1>Customer</h1>;
+};
+ 
+export default Customers;
---
+++ typescript/pages/employees.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Employees: NextPage = () => {
+    return <h1>Wmployee</h1>
+};
+
+export default Employees;
