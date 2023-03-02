3818531c2603f2079e3b1f0817b7cb99febcc7e9
Eniekpo <johneniekpo4life@gmail.com>
2023-03-02 13:34:49 +0100

    a

--- typescript/pages/customers/[id]/orders/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const Orders: NextPage = () => {
-    return <h1>All Orders</h1>;
-};
-
-export default Orders;
--- typescript/pages/customers/index.tsx
+++ typescript/pages/customers/index.tsx
@@ -1,8 +1,27 @@
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
+export const getStaticProps: GetStaticProps = async (context) =>{
+    return {
+        props: {
+            customers: [
+                {
+                    id: 1,
+                    name: "John Smith",
+                    industry: "Restaurant"
+                },
+                {
+                    id: 2,
+                    name: "Sal Brown",
+                    industry: "Tech industry"
+                },
+            ]
+        }
+    }
+}
 
-const Customers: NextPage = () => {
-    
+const Customers: NextPage = (props) => {
+    console.log(props);
+
     return <h1>Customers</h1>;
 };
- 
+
 export default Customers;
