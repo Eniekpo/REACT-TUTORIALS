7e24d0cb6e96da4c8bf8c23bd328bca9a04ce357
Eniekpo <johneniekpo4life@gmail.com>
2023-03-19 23:21:03 +0100

    a

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,26 +1,6 @@
-import Header from "./components/Header";
-import Employees from "./pages/Employees";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Customers from "./pages/Customers";
-import Dictionary from './pages/Dictionary';
-import Definition from "./pages/Definition";
-import NotFound from "./components/NotFound";
-
 function App() {
   return (
-    <Router>
-      <Header>
-        <Routes>
-          <Route path='/' element={<Employees />} />
-          <Route path='/customers' element={<Customers />} />
-          <Route path='/dictionary' element={<Dictionary />} />
-          <Route path='/definition' element={<Definition />} />
-          <Route path='/404' element={<NotFound />} />
-          <Route path='*' element={<NotFound />} />
-          <Route path='/definition/:search' element={<Definition />} />
-        </Routes>
-      </Header>
-    </Router>
+  <div>Hello</div>
   )
 
 }
